8a5b9e14504683668be73195e4e817b2eca30469
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-08 14:21:30 +0530

    Update data.js

--- data.js
+++ data.js
@@ -19,25 +19,3 @@ let results = [
     rank: "1"
   }
 ];
-
-let results = [
-  {
-    roll: "102",
-    name: "Aman Zayan",
-    class: "8",
-
-    subjects: {
-      Fiqh: 90,
-      Thareeq: 70,
-      Duroos: 60,
-      Lisan: 75
-    },
-
-    total: 300,
-    percentage: "75.5%",
-    ajar: 134,
-
-    examDate: "10 March 2026",
-    rank: "2"
-  }
-    
