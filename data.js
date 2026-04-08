dc5c9b7227c68ecda683fe24f8aebe04d1834bf1
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-08 06:20:56 +0530

    Fix duplicate entries and update student names

--- data.js
+++ data.js
@@ -1,13 +1,12 @@
 let results = [
   {roll: "101", name: "Fowzan", class: "8", mark: "85%"},
-  {roll: "102", name: "Hazeem Assan", class: "8", mark: "90%"}
-  {roll: "103", name: "Hazeem Assan", class: "8", mark: "90%"
-  {roll: "104", name: "Aman Zayan", class: "8", mark: "90%"
-  {roll: "105", name: "Izan Midlaj", class: "8", mark: "90%"
-  {roll: "106", name: "Muhammed Razin Razak", class: "8", mark: "90%"
-  {roll: "107", name: "Muhammed Naji", class: "8", mark: "90%"
-  {roll: "108", name: "Fella Fasil", class: "8", mark: "90%"
-  {roll: "109", name: "Delfa Fathima", class: "8", mark: "90%"
-  {roll: "110", name: "Iza Fathima", class: "8", mark: "90%"
-  {roll: "111", name: "Badriyya Febim", class: "8", mark: "90%"
+  {roll: "102", name: "Hazeem Assan", class: "8", mark: "90%"},
+  {roll: "103", name: "Aman Zayan", class: "8", mark: "90%"},
+  {roll: "104", name: "Izan Midlaj", class: "8", mark: "90%"},
+  {roll: "105", name: "Muhammed Razin Razak ", class: "8", mark: "90%"},
+  {roll: "106", name: "Muhammed Naji", class: "8", mark: "90%"},
+  {roll: "107", name: "Fella Fasil", class: "8", mark: "90%"},
+  {roll: "108", name: "Delfa Fathima", class: "8", mark: "90%"},
+  {roll: "109", name: "Iza Fathima", class: "8", mark: "90%"},
+  {roll: "110", name: "Badriyya Febin", class: "8", mark: "90%"},
     ];
