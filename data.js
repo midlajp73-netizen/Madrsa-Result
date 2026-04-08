cf836bcc04655346290c8a55b7fb18dd75b2ea38
DARUL HUDA -9547 <midlajp73@gmail.com>
2026-04-08 06:43:54 +0530

    Refactor results to include detailed student information
    Updated student data structure to include subjects, total, percentage, and ajar.

--- data.js
+++ data.js
@@ -1,12 +1,35 @@
 let results = [
-  {roll: "101", name: "Fowzan", class: "8", mark: "85%"},
-  {roll: "102", name: "Hazeem Assan", class: "8", mark: "90%"},
-  {roll: "103", name: "Aman Zayan", class: "8", mark: "90%"},
-  {roll: "104", name: "Izan Midlaj", class: "8", mark: "90%"},
-  {roll: "105", name: "Muhammed Razin Razak ", class: "8", mark: "90%"},
-  {roll: "106", name: "Muhammed Naji", class: "8", mark: "90%"},
-  {roll: "107", name: "Fella Fasil", class: "8", mark: "90%"},
-  {roll: "108", name: "Delfa Fathima", class: "8", mark: "90%"},
-  {roll: "109", name: "Iza Fathima", class: "8", mark: "90%"},
-  {roll: "110", name: "Badriyya Febin", class: "8", mark: "90%"},
-    ];
+  {
+    roll: "101",
+    name: "Muhammed Fawzan",
+    class: "8",
+
+    subjects: {
+      Quran: 90,
+      Arabic: 85,
+      English: 80,
+      Malayalam: 75
+    },
+
+    total: 450,
+    percentage: "82.5%",
+    ajar: 134
+  },
+
+  {
+    roll: "102",
+    name: "Hafsa",
+    class: "6",
+
+    subjects: {
+      Quran: 95,
+      Arabic: 88,
+      English: 84,
+      Malayalam: 80
+    },
+
+    total: 347,
+    percentage: "86.7%",
+    ajar: 140
+  }
+];
